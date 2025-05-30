a025129304e112a0fd86c83f8460626a93870f3b
SntJhn <jagdevera@gmail.com>
2025-05-30 22:43:11 +0800

    Add: Frontend

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="w-[100vw] h-[100vh] relative bg-slate-950 overflow-hidden">
+            <div className="w-60 h-52 left-[1128px] top-[116px] absolute bg-fuchsia-900/70 rounded-full blur-3xl" />
+            <div className="w-[592.86px] h-[532.01px] left-[38px] top-[-202px] absolute origin-top-left rotate-[2.88deg] bg-blue-700/40 blur-3xl" />
+            <div className="w-[614.08px] h-[611.58px] left-[1536px] top-[1186.64px] absolute origin-top-left rotate-[176.83deg] bg-blue-700/40 blur-3xl" />
+            <div className="w-48 h-40 left-[134px] top-[685px] absolute bg-fuchsia-900 rounded-full blur-3xl" />
+            <div className="w-40 h-28 left-[-93px] top-[-74px] absolute bg-red-900/80 rounded-full blur-3xl" />
+            <div className="w-32 h-28 left-[1180px] top-[231px] absolute bg-red-900/95 rounded-full blur-3xl" />
+            <div className="w-32 h-28 left-[233px] top-[702px] absolute bg-red-900/70 rounded-full blur-3xl" />
+        </div>
+    );
+};
+
+export default Home;

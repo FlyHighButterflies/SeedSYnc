7b00b46e54c13534b41b90bf4d651baab19efc0f
Reymel Sardenia <sardeniareymel@gmail.com>
2025-06-27 19:29:29 +0800

    feat: boyerMoore Implementation

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,7 @@ import cropRoutes from './cropRoutes.js';
 import inventoryRoutes from './inventoryRoutes.js';
 import reviewRoutes from './reviewRoutes.js';
 import tradeRoutes from './tradeRoutes.js';
+import searchRoutes from './searchRoutes.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use('/crops', cropRoutes);
 router.use('/inventory', inventoryRoutes);
 router.use('/reviews', reviewRoutes);
 router.use('/trades', tradeRoutes);
+router.use('/', searchRoutes);
 
 export default router;

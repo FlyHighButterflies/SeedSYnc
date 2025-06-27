373f624900a6df5d9d1d19d0c42e516495c21675
SntJhn <jagdevera@gmail.com>
2025-06-27 21:56:04 +0800

    Merge: BoyerMoore

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cropRoutes from "./cropRoutes.js";
-
+import inventoryRoutes from "./inventoryRoutes.js";
+import matchRoutes from "./matchRoutes.js";
+import messageRoutes from "./messageRoutes.js";
+import chatLogRoutes from "./chatLogRoutes.js";
+import notificationRoutes from "./notificationRoutes.js";
 import reviewRoutes from "./reviewRoutes.js";
 import tradeRoutes from "./tradeRoutes.js";
 import searchRoutes from "./searchRoutes.js";

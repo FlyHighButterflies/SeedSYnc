11b16dbad18c79d97022a73218aff1891e9dd6d3
Alingggg <lapitangeof2@gmail.com>
2025-06-27 17:32:35 +0800

    feat: inventory page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import {
     Route,
     Navigate,
 } from "react-router-dom";
-import { Home, SignUp, Login, Listings } from "@/pages";
+import { Home, SignUp, Login, Listings, Inventory } from "@/pages";
 import {
     AuthLayout,
     DashboardLayout,
@@ -86,7 +86,7 @@ function App() {
                         element={
                             <ProtectedRoute>
                                 <DashboardLayout>
-                                    <div>Inventory Page</div>
+                                    <Inventory />
                                 </DashboardLayout>
                             </ProtectedRoute>
                         }

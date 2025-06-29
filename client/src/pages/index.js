2059778d72871fc9ea5822fed8dd28e11fd55f3c
Alingggg <lapitangeof2@gmail.com>
2025-06-29 19:44:59 +0800

    Update index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,6 +5,5 @@ export { default as Listings } from "./Listings";
 export { default as Inventory } from "./Inventory";
 export { default as Profile } from "./Profile";
 export { default as OnboardingPage1 } from "./OnboardingPage1";
-export { default as OnboardingPage2 } from "./OnboardingPage2";
 export { default as UserProfile } from "./UserProfile";
 export { default as Messages } from "./Messages";
